9c754f772ebdf9f83a6dc58e6c1ac344b7d25a20
Ondřej <ondrej@pop-os.home>
2025-12-27 20:59:11 +0100

    added members table creation

--- books.sql
+++ books.sql
@@ -12,14 +12,17 @@ CREATE TABLE authors (
 )
 
 CREATE TABLE books (
-    book_id SERIAL NOT NULL PRIMARY KEY
+    book_id SERIAL NOT NULL PRIMARY KEY,
     title VARCHAR(200) NOT NULL,
     published_year YEAR,
     author_id INT REFERENCES authors(author_id)
 )
 
 CREATE TABLE members (
-
+    member_id SERIAL NOT NULL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(150) UNIQUE NOT NULL,
+    joined_date DATE DEFAULT CURRENT_DATE
 )
 
 CREATE TABLE loans (
